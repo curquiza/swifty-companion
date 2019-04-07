c8459a7789e348cb387be73a0a7abe9318598f3d
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-04-07 15:47:52 +0200

    Add project structure with pods

---
+++ swifty-companion/swifty-companion/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Clementine URQUIZAR on 4/7/19.
+//  Copyright © 2019 Clementine URQUIZAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // curl  -H "Authorization: Bearer TOKEN" "https://api.intra.42.fr/v2/users/curquiza"
+    
+    
+}
+
