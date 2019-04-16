af5bd78420a6343918557c6ffc1272272e2afd03
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-04-16 13:02:25 +0200

    add basic view wth photo

--- swifty-companion/swifty-companion/ProfileViewController.swift
+++ swifty-companion/swifty-companion/ProfileViewController.swift
@@ -13,12 +13,32 @@ class ProfileViewController: UIViewController {
     
     var user: User?
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         print("[Profile view loaded]")
-        
-        print("user = \(user)")
-        
         title = "Profile"
+        
+        profileImageView.contentMode = .scaleAspectFit
+        self.profileImageView.image = stringURLToUIImage(stringURL: user?.image_url)
+        
+        guard let image = UIImage(named: "42_background") else { return }
+        self.view.backgroundColor = UIColor(patternImage: image)
     }
+    
+    func stringURLToUIImage(stringURL: String?) -> UIImage? {
+        guard let str = stringURL else { return nil }
+        guard let imageURL = URL(string: str) else {return nil}
+        if let imageData: NSData = NSData(contentsOf: imageURL) {
+            return UIImage(data: imageData as Data)
+        }
+        return nil
+    }
+    
+
 }
 
